15bc202c35cd9bc209eb0ce80e8a5474e815298f
tyem123ks2 <thanhtuan02051011@gmail.com>
2022-10-06 16:24:13 +0700

    UPDATE

--- src/ss2_loop_java/practice/Exam_isPrime100.java
+++ src/ss2_loop_java/practice/Exam_isPrime100.java
@@ -1,12 +1,23 @@
 package ss2_loop_java.practice;
 
-import java.util.Scanner;
-
 public class Exam_isPrime100 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int A = 2;
-        double s = Math.sqrt(A);
-
+        int i = 0;
+        int num = 0;
+        //;
+        String primeNumber = "";
+        for (i = 1; i <= 100; i++) {
+            int count = 0;
+            for (num = i; num >= 1; num--) {
+                if (i % num == 0) {
+                    count = count + 1;
+                }
+            }
+            if (count == 2) {
+                primeNumber = primeNumber + i + " ";
+            }
+        }
+        System.out.println("Các số nguyên tố từ 1 đến 100 là: ");
+        System.out.println(primeNumber);
     }
 }
--- src/ss3_array_java/practice/Exam1_deleteArray.java
+++ src/ss3_array_java/practice/Exam1_deleteArray.java
@@ -1,4 +1,24 @@
 package ss3_array_java.practice;
 
+import java.util.Scanner;
+
 public class Exam1_deleteArray {
+    public static void main(String[] args) {
+        int size;
+        int array[];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size: ");
+        size = scanner.nextInt();
+        int i = 0;
+        array = new int[size];
+        while (i < array.length) {
+            System.out.println("Enter element " + (i + 1) + ":");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        System.out.println("MẢng vừa nhập là: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+    }
 }
