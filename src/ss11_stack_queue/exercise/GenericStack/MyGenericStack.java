b53c95edc429314ac01d85d67faea571e0a55437
tyem123ks2 <thanhtuan02051011@gmail.com>
2022-10-19 10:55:12 +0700

    UPDATE

---
+++ src/ss11_stack_queue/exercise/GenericStack/MyGenericStack.java
@@ -0,0 +1,37 @@
+package ss11_stack_queue.exercise.GenericStack;
+
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
+
+    public MyGenericStack() {
+        stack = new LinkedList<>();
+    }
+
+    public void push(T element) {
+        stack.addFirst(element);
+    }
+
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+
+    public int size() {
+        return stack.size();
+    }
+
+    boolean isEmpty() {
+        if (stack.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
