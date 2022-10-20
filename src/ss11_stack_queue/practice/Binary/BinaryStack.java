53cc12c75cde67d4ce0c7312b8d19d0be0a9b1d4
tyem123ks2 <thanhtuan02051011@gmail.com>
2022-10-20 07:34:34 +0700

    UPDATE

---
+++ src/ss11_stack_queue/practice/Binary/BinaryStack.java
@@ -0,0 +1,19 @@
+package ss11_stack_queue.practice.Binary;
+
+import java.util.Stack;
+
+public class BinaryStack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack();
+        int n = 13;
+
+        while (n > 0) {
+            stack.push(n % 2);
+            n /= 2;
+        }
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+        System.out.println(stack);
+    }
+}
